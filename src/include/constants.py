49234ee210c3006ef2b437b716eb973a0b76035c
Creeper19472 <creeper19472@crpteam.club>
2026-03-13 23:01:58 +0800

    feat(username): limit username length to 64 characters in database schema

--- src/include/constants.py
+++ src/include/constants.py
@@ -15,7 +15,7 @@ __all__ = [
     "ROOT_DIRECTORY_ID",
 ]
 
-CORE_VERSION = Version("0.1.0.260312_alpha")
+CORE_VERSION = Version("0.1.0.260313_alpha")
 PROTOCOL_VERSION = 9
 
 AVAILABLE_ACCESS_TYPES = ["read", "write", "move", "manage"]
